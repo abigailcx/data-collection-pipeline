f509ffc880ccbf6879a9ff6290eeeeefd5ea3025
Abigail Cox <abigailgcox@outlook.com>
2022-12-16 00:50:28 +0000

    adds functional scraper, including image retrieval

--- webscraper_selenium_zoopla.py
+++ webscraper_selenium_zoopla.py
@@ -1,17 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from datetime import datetime
+import collections
+import os
 import time
 import yaml
-import collections
-
-# Sale Price: Our response variable
-# Number of bedrooms
-# Square footage
-# Description
-# Address
-
-#class Config:
-    
+import json
+import pandas as pd
+import urllib
 
 class Scraper:
     
@@ -47,82 +43,101 @@ class Scraper:
 
 
     def get_urls(self):
-        XPATH_FOR_REGULAR_PROP_PAGE_LIST = self.get_config()['xpath_for_regular_prop_page_list']
+        # XPATH_FOR_REGULAR_PROP_PAGE_LIST = self.get_config()['xpath_for_regular_prop_page_list']
         # XPATH_FOR_FEATURED_PROP_PAGE_LIST = self.get_config()['xpath_for_featured_prop_page_list']
-        
+        xpath_regular_listings = '//div[@data-testid="regular-listings"]'
+        regular_listings = self.driver.find_element(by=By.XPATH, value=xpath_regular_listings)
+        regular_listings_class_attribute = regular_listings.get_attribute('class')
+        self.driver
         time.sleep(2)
-        regular_prop_container = self.driver.find_element(by=By.XPATH, value=XPATH_FOR_REGULAR_PROP_PAGE_LIST) # Change this xpath with the xpath the current page has in their properties
+        regular_prop_container = self.driver.find_element(by=By.XPATH, value=f'//div[@class="{regular_listings_class_attribute}"]') # Change this xpath with the xpath the current page has in their properties
         regular_prop_list = regular_prop_container.find_elements(by=By.XPATH, value='./div')
-        print(regular_prop_list)
-        # featured_prop_container = self.driver.find_element(by=By.XPATH, value=XPATH_FOR_FEATURED_PROP_PAGE_LIST) # Change this xpath with the xpath the current page has in their properties
-        # featured_prop_list = featured_prop_container.find_elements(by=By.XPATH, value='./div')
-
-        # prop_list = regular_prop_list + featured_prop_list
 
         for prop in regular_prop_list:
             reg_a_tag = prop.find_element(by=By.TAG_NAME, value='a')
             reg_link = reg_a_tag.get_attribute('href')
             self.prop_url_list.append(reg_link)
 
-        # for prop in featured_prop_list:
-        #     feat_a_tag = featured_prop_container.find_element(by=By.TAG_NAME, value='a')
-        #     feat_link = feat_a_tag.get_attribute('href')
-        #     self.prop_url_list.append(feat_link)
-        
-        print(self.prop_url_list)
-        print(len(self.prop_url_list))
-
         return self.prop_url_list
 
+
     def extract_prop_data(self):
-        print(self.prop_url_list)
+
         for num, url in enumerate(self.prop_url_list, start=1):
             self.driver.get(url)
             time.sleep(2) # Wait a couple of seconds, so the website doesn't suspect you are a bot
+            
             XPATH_SUMMARY = self.get_config()['xpath_summary']
             XPATH_PRICE = self.get_config()['xpath_price']
             XPATH_ADDRESS = self.get_config()['xpath_address']
-            XPATH_NUM_BEDROOMS = self.get_config()['xpath_num_bedrooms']
-           # XPATH_NUM_BATHROOMS = self.get_config()['xpath_num_bathrooms']
+            XPATH_ROOM_COUNT = self.get_config()['xpath_room_count']
+            XPATH_NUM_ROOMS = self.get_config()['xpath_num_rooms']
+            XPATH_IMAGE_GALLERY = self.get_config()['xpath_image_gallery']
+            XPATH_IMAGES = self.get_config()['xpath_images']
 
             summary = self.driver.find_element(by=By.XPATH, value=XPATH_SUMMARY)
             price = self.driver.find_element(by=By.XPATH, value=XPATH_PRICE)
             address = self.driver.find_element(by=By.XPATH, value=XPATH_ADDRESS)
-            #num_bedrooms = self.driver.find_element(by=By.XPATH, value=XPATH_NUM_BEDROOMS)
-            #num_bathrooms = self.driver.find_element(by=By.XPATH, value=XPATH_NUM_BATHROOMS)
-            
+            room_count = self.driver.find_elements(by=By.XPATH, value=XPATH_ROOM_COUNT)
+            image_gallery_elements = self.driver.find_elements(by=By.XPATH, value=XPATH_IMAGE_GALLERY)
+
+            room_info = []
+            for elem in room_count:
+                room_info.append(elem.find_element(by=By.XPATH, value=XPATH_NUM_ROOMS).text)
+          
             self.prop_dict[num]['summary'] = summary.text
             self.prop_dict[num]['price'] = price.text
             self.prop_dict[num]['address'] = address.text
-            #self.prop_dict[num]['num_bedrooms'] = num_bedrooms.text 
+            self.prop_dict[num]['num_rooms'] = room_info
             self.prop_dict[num]['link'] = url
-            #self.prop_dict[num]['num_bathrooms'] = num_bathrooms.text
-            #self.prop_dict[num]['num_receptions'] = num_receptions.text
+            
+            img_src = []
+            for img in image_gallery_elements:
+                print(img)
+                img_src.append(img.find_element(by=By.XPATH, value=XPATH_IMAGES).get_attribute('src'))
+                
+                try:
+                    next_image_button = self.driver.find_element(by=By.XPATH, value='//button[@data-testid="arrow_right"]')
+                    next_image_button.click()
+                    time.sleep(2)
+                except:
+                    print("There is no next button on this page.")
+                    pass
+
+            #breakpoint()
+            for idx, url in enumerate(img_src, start=1):
+                filedir_img = f"./image_data/{datetime.now().strftime('%d-%m-%Y')}/{num}_{address.text}/{idx}.jpg"
+                os.makedirs(os.path.dirname(filedir_img), exist_ok=True)
+                urllib.request.urlretrieve(url, filedir_img)
         
         print(self.prop_dict)
-    
+
+
     def get_csv(self):
-        #TODO: read property data into a csv and save out
-        pass
+        filedir_csv = f"./csv_data/{datetime.now().strftime('%d-%m-%Y_%H%M%S')}_{self.get_config()['csv_outfile']}"
+        os.makedirs(os.path.dirname(filedir_csv), exist_ok=True)
+        df = pd.DataFrame(self.prop_dict)
+        df = df.transpose()
+        df.to_csv(filedir_csv)
+
 
     def get_json(self):
-        #TODO: read property data into a json and save out
-        pass
+        prop_data = json.dumps(self.prop_dict, sort_keys=False, indent=4, ensure_ascii=False)
+        filedir_json = f"./json_data/{datetime.now().strftime('%d-%m-%Y_%H%M%S')}_{self.get_config()['json_outfile']}"
+        os.makedirs(os.path.dirname(filedir_json), exist_ok=True)
+        with open(filedir_json, 'w') as outfile:
+            outfile.write(prop_data)
+
+
+    def scrape(self):
+        self.get_config()
+        self.handle_cookies()
+        self.get_urls()
+        self.extract_prop_data()
+
 
 if __name__ == "__main__":
     s = Scraper('config.yaml')
-    s.get_config()
-    s.handle_cookies()
-    s.get_urls()
-    s.extract_prop_data()
-
-# TODO:
-# sort out issue with the xpaths to get the list of links for specific property listings
-# get list of links
-# for each link in list, do scraping to find:
-# price
-# address label
-# general tagline
-# num bedrooms
-# num bathrooms
-
+    s.scrape()
+    s.get_json()
+    s.get_csv()
