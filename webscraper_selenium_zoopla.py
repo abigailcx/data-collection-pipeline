3a0784f1ad390b150080a20d700fd83613c93633
Abigail Cox <abigailgcox@outlook.com>
2023-01-07 16:26:15 +0000

    updates extract_prop_data to include conditional flow for whether or not to extract image data.

--- webscraper_selenium_zoopla.py
+++ webscraper_selenium_zoopla.py
@@ -1,16 +1,20 @@
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from datetime import datetime
 import collections
-import os
-import time
-import yaml
 import json
+import os
 import pandas as pd
+import time
 import urllib
+import yaml
+
 
 class Scraper:
-    
+    """
+
+    """
+
     def __init__(self, config_file):
         self.config_file = config_file
         self.driver = webdriver.Chrome()
@@ -18,12 +22,24 @@ class Scraper:
         self.prop_dict = collections.defaultdict(dict)
 
     def get_config(self):
+        """
+        A method to retrieve configuration information from config.yaml file such as:
+        - which url to scrape
+        - how many properties to get data for
+        - names of json and csv output files
+        - all necessary xpaths
+        """
+
         with open(self.config_file, 'r') as f:
             config_dict = yaml.load(f, Loader=yaml.FullLoader)
         
         return config_dict
 
     def handle_cookies(self):
+        """
+        A method to accept the cookies pop-up window 
+        """
+
         URL = f"{self.get_config()['url']}&page_size={self.get_config()['page_size']}"
         self.driver.get(URL)
         time.sleep(2) # Wait a couple of seconds, so the website doesn't suspect you are a bot
@@ -43,6 +59,9 @@ class Scraper:
 
 
     def get_urls(self):
+        """
+        A method to retrieve the urls for each property 
+        """
         # XPATH_FOR_REGULAR_PROP_PAGE_LIST = self.get_config()['xpath_for_regular_prop_page_list']
         # XPATH_FOR_FEATURED_PROP_PAGE_LIST = self.get_config()['xpath_for_featured_prop_page_list']
         xpath_regular_listings = '//div[@data-testid="regular-listings"]'
@@ -62,7 +81,9 @@ class Scraper:
 
 
     def extract_prop_data(self):
+        """
 
+        """
         for num, url in enumerate(self.prop_url_list, start=1):
             self.driver.get(url)
             time.sleep(2) # Wait a couple of seconds, so the website doesn't suspect you are a bot
@@ -72,15 +93,12 @@ class Scraper:
             XPATH_ADDRESS = self.get_config()['xpath_address']
             XPATH_ROOM_COUNT = self.get_config()['xpath_room_count']
             XPATH_NUM_ROOMS = self.get_config()['xpath_num_rooms']
-            XPATH_IMAGE_GALLERY = self.get_config()['xpath_image_gallery']
-            XPATH_IMAGES = self.get_config()['xpath_images']
-
+            
             summary = self.driver.find_element(by=By.XPATH, value=XPATH_SUMMARY)
             price = self.driver.find_element(by=By.XPATH, value=XPATH_PRICE)
             address = self.driver.find_element(by=By.XPATH, value=XPATH_ADDRESS)
             room_count = self.driver.find_elements(by=By.XPATH, value=XPATH_ROOM_COUNT)
-            image_gallery_elements = self.driver.find_elements(by=By.XPATH, value=XPATH_IMAGE_GALLERY)
-
+            
             room_info = []
             for elem in room_count:
                 room_info.append(elem.find_element(by=By.XPATH, value=XPATH_NUM_ROOMS).text)
@@ -90,30 +108,41 @@ class Scraper:
             self.prop_dict[num]['address'] = address.text
             self.prop_dict[num]['num_rooms'] = room_info
             self.prop_dict[num]['link'] = url
-            
-            img_src = []
-            for img in image_gallery_elements:
-                print(img)
-                img_src.append(img.find_element(by=By.XPATH, value=XPATH_IMAGES).get_attribute('src'))
-                
-                try:
-                    next_image_button = self.driver.find_element(by=By.XPATH, value='//button[@data-testid="arrow_right"]')
-                    next_image_button.click()
-                    time.sleep(2)
-                except:
-                    print("There is no next button on this page.")
-                    pass
 
-            #breakpoint()
-            for idx, url in enumerate(img_src, start=1):
-                filedir_img = f"./image_data/{datetime.now().strftime('%d-%m-%Y')}/{num}_{address.text}/{idx}.jpg"
-                os.makedirs(os.path.dirname(filedir_img), exist_ok=True)
-                urllib.request.urlretrieve(url, filedir_img)
-        
-        print(self.prop_dict)
+            if self.get_config()['extract_image_data']:
+                XPATH_IMAGE_GALLERY = self.get_config()['xpath_image_gallery']
+                XPATH_IMAGES = self.get_config()['xpath_images']
+
+                image_gallery_elements = self.driver.find_elements(by=By.XPATH, value=XPATH_IMAGE_GALLERY)
+            
+                img_src = []
+                # appends the src url to img_src as a list 
+                for img in image_gallery_elements:
+                    print(img)
+                    img_src.append(img.find_element(by=By.XPATH, value=XPATH_IMAGES).get_attribute('src'))
+                
+                    try:
+                        next_image_button = self.driver.find_element(by=By.XPATH, value='//button[@data-testid="arrow_right"]')
+                        next_image_button.click()
+                        time.sleep(2)
+                    
+                    except:
+                        print("There is no next button on this page.")
+                        pass
+
+                for idx, url in enumerate(img_src, start=1):
+                    filedir_img = f"./image_data/{datetime.now().strftime('%d-%m-%Y')}/{num}_{address.text}/{idx}.jpg"
+                    os.makedirs(os.path.dirname(filedir_img), exist_ok=True)
+                    urllib.request.urlretrieve(url, filedir_img)
+            
+            else:
+                continue
 
 
     def get_csv(self):
+        """
+        A method to create a csv file of scraped property data
+        """
         filedir_csv = f"./csv_data/{datetime.now().strftime('%d-%m-%Y_%H%M%S')}_{self.get_config()['csv_outfile']}"
         os.makedirs(os.path.dirname(filedir_csv), exist_ok=True)
         df = pd.DataFrame(self.prop_dict)
@@ -122,6 +151,10 @@ class Scraper:
 
 
     def get_json(self):
+        """
+        A method to create a json file of scraped property data
+        """
+
         prop_data = json.dumps(self.prop_dict, sort_keys=False, indent=4, ensure_ascii=False)
         filedir_json = f"./json_data/{datetime.now().strftime('%d-%m-%Y_%H%M%S')}_{self.get_config()['json_outfile']}"
         os.makedirs(os.path.dirname(filedir_json), exist_ok=True)
@@ -130,6 +163,9 @@ class Scraper:
 
 
     def scrape(self):
+        """
+
+        """
         self.get_config()
         self.handle_cookies()
         self.get_urls()
@@ -139,5 +175,6 @@ class Scraper:
 if __name__ == "__main__":
     s = Scraper('config.yaml')
     s.scrape()
-    s.get_json()
     s.get_csv()
+    s.get_json()
+    
